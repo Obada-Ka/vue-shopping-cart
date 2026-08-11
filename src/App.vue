549982a8c752630dad3e7cca768d8bcc56674fe9
Obada Karaali <obadakaraali@gmail.com>
2026-08-11 22:53:30 +0200

    display toast for Proceeding to checkout!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Toaster } from 'vue-sonner'
+</script>
 
 <template>
+  <Toaster position="top-right" rich-colors />
   <main class="mx-auto min-h-fit w-full flex-1 px-10 lg:my-8 lg:px-20">
     <router-view />
   </main>
